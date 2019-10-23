22c1403bfff1de0c22e5d7277661942219105efe
ben <ben@benwal.io>
2019-10-23 16:56:00 -0400

    initial commit

---
+++ ip-check.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+import os.path
+import subprocess
+import bw_pushover
+
+IP_API = 'https://api.ipify.org?format=json'
+OLD_IP_FILE_PATH = '/etc/external_ip'
+CLOUDFLARE_UPDATE_SCRIPT_PATH = '/home/pi/.scripts/cf-dns-update/cf-dns-update.py'
+
+def get_updated_ip():
+    resp = requests.get(IP_API)
+    json_data = json.loads(resp.text)
+    IP_ADDRESS = json_data['ip']
+    # print(IP_ADDRESS)
+    return IP_ADDRESS
+
+def update_ip(ip_address):
+    f = open(OLD_IP_FILE_PATH, 'w')
+    f.write(ip_address)
+    f.close
+
+f = open(OLD_IP_FILE_PATH, 'r')
+oldip = f.read()
+f.close()
+
+newip = get_updated_ip()
+
+if not os.path.exists(OLD_IP_FILE_PATH):
+    update_ip(newip)
+elif oldip != newip:
+    update_ip(newip)
+    subprocess.call(['python3',CLOUDFLARE_UPDATE_SCRIPT_PATH])
+    title = "redpi - update IP"
+    message = "SUCCESS - IP changed from {} to {}".format(oldip,newip)
+    bw_pushover.send_message(message, title)
---
+++ secrets.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+# secrets file
+cloudflare_api_key = '4fd271e8d15a537d606a3eca5118d38110720'
+cloudflare_email = 'ben.wallace@protonmail.ch'
+cloudflare_zone_id = '3effeff0b890a8940b6276af08822b37'
+cloudflare_dns_record_ids = ['2d3a58771c1f3c6cf4b1688a5cf74ace','c12a776a583316f3f17de70c07291e28','de490614cb7a5d17a36ee1efe696db59','c4056cac9e476a4937d18f6affdac4fd']
+pushover_user_key = 'uhv3jxpm8y2q6eqe12126ahhcjudmu'
+pushover_app_token = 'aqbsmdax5n2kqdadh6fs4bm1g6ucer'
